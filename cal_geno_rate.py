ed7ce063560d83e3e9455720e030e3fd39fc24e5
393099545@qq.com <cmiao@apple.(none)>
2015-03-12 02:20:32 +0800

    each genotype rate in sample and whole population

---
+++ cal_geno_rate.py
@@ -0,0 +1,39 @@
+#!/usr/lib/python
+
+def calhrate(map_file):
+    f1 = open(map_file)
+    fir_line = f1.readline()
+    sm_info = fir_line.split()[1:]
+    sm_seq = map(list,zip(*(i.split()[1:] for i in f1)))
+    total_h, total_a, total_b = 0, 0, 0
+    for i,j in zip(sm_info,sm_seq):
+        print 'for sample: %s'%i
+        t_n = 0
+        t_h_n = 0
+        t_a_n = 0
+        t_b_n = 0
+        for m in j:
+            t_n += 1
+            if m == 'X':
+                t_h_n += 1
+            if m == 'A':
+                t_a_n += 1
+            if m == 'B':
+                t_b_n += 1
+        total_h += t_h_n
+        total_a += t_a_n
+        total_b += t_b_n
+        print 'X rate: %s'%(t_h_n/float(t_n))
+        print 'A rate: %s'%(t_a_n/float(t_n))
+        print 'B rate: %s'%(t_b_n/float(t_n))
+    T = float(total_h+total_a+total_b)
+    print 'For whole population:\nA: %s\nB: %s\nX: %s'%(total_a/T, total_b/T, total_h/T)
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) == 2:
+        calhrate(sys.argv[1])
+    else:
+        print 'usage:\npython cal_hrate.py mapfile ho_a ho_b he_h'
+
+
