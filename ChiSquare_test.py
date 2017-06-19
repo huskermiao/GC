26ae8d34d8353ca2f39b238ca035e3a4ed611ac0
Chenyong Miao <cmiao@huskers.unl.edu>
2017-06-19 18:46:23 -0500

    Update ChiSquare_test.py

--- ChiSquare_test.py
+++ ChiSquare_test.py
@@ -18,31 +18,37 @@ matrix file please see our wiki page:
 (https://github.com/freemao/Genotype-corrector/wiki/Genotype-Corrector).''')
 optparser.add_option('-p', '--population_type', dest = 'population',
                      help = "set the population type, F2 or RIL")
-optparser.add_option('-d', '--degree', dest = 'degree_level',
+optparser.add_option('-d', '--degree', dest = 'degree_level', default = 1,
                      help = '''integer, 1 corresponds to the the pvalue of 1e-11(less strigenct)
 for chisquare test, 10 corresponds to 1e-2 (more strigent). Users can test
 different degrees(1-10) to get a reseanable result''')
 optparser.add_option('-o', '--output', dest = 'output_filename',
                      help = 'Write the filtered results to this file.')
+optparser.add_option('-a', '--homo1', dest = 'homozygous1', default = 'a',
+                     help = 'character for homozygous genotype.')
+optparser.add_option('-b', '--homo2', dest = 'homozygous2', default = 'b',
+                     help = 'character for alternative homozygous genotype.')
+optparser.add_option('-c', '--heter', dest = 'heterozygous', default = 'h',
+                     help = 'character for heterozygous genotype.')
 options, args = optparser.parse_args()
 
 import numpy as np
 
-def F2chitest(mapfile, p_value, output):
+def F2chitest(mapfile, p_value, output, A, B):
     f2 = open(output, 'w')
     expect_rate = np.array([0.5, 0.5])
     f = open(mapfile)
     firline = f.readline()
     f2.write(firline)
     for i in f:
-        a_count = i.count('\tA')
-        b_count = i.count('\tB')
+        a_count = i.count('\t%s'%A)
+        b_count = i.count('\t%s'%B)
         observed = np.array([a_count, b_count])
         cvalue, pvalue = chisquare(observed, expect_rate*np.sum(observed))
         if pvalue > p_value:
             f2.write(i)
 
-def RILchitest(mapfile, p_value, output):
+def RILchitest(mapfile, p_value, output, A, B, C):
     f2 = open(output, 'w')
     expect_rate = np.array([0.5, 0.5])
     f = open(mapfile)
@@ -50,15 +56,15 @@ def RILchitest(mapfile, p_value, output):
     f2.write(firline)
     N = len(firline.split()[1:])
     for i in f:
-        a_count = i.count('\tA')
-        b_count = i.count('\tB')
-        x_count = i.count('\tX')
+        a_count = i.count('\t%s'%A)
+        b_count = i.count('\t%s'%B)
+        x_count = i.count('\t%s'%C)
         hetero_rate = x_count/float(N)
         if hetero_rate <= 0.5:
             observed = np.array([a_count, b_count])
             cvalue, pvalue = chisquare(observed, expect_rate*np.sum(observed))
             if pvalue > p_value:
-                print a_count,b_count
+               # print a_count,b_count
                 f2.write(i)
 
 if __name__ == "__main__":
@@ -66,13 +72,16 @@ if __name__ == "__main__":
     P = options.population
     D = options.degree_level
     O = options.output_filename
-    if I and P and D and O:
+    A = options.homozygous1
+    B = options.homozygous2
+    C = options.heterozygous
+    if I and P and D and O and A and B and C:
         Pvalue = float('1e%s'%(int(D)-12))
         print Pvalue
         if P == 'F2':
-            F2chitest(I,Pvalue, O )
+            F2chitest(I,Pvalue, O, A, B, C )
         elif P == 'RIL':
-            RILchitest(I,Pvalue, O )
+            RILchitest(I,Pvalue, O, A, B, C)
         else:
             print 'Add -h to show help.'
     else:
