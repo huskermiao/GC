c41904e4198e4edaf5dbc906487027ef66758224
cmiao@huskers.unl.edu <cmiao@huskers.unl.edu>
2017-11-05 21:56:06 -0600

    update

--- vcf2map.py
+++ vcf2map.py
@@ -4,47 +4,19 @@
 import vcf
 from optparse import OptionParser
 
-msg_usage = 'usage: %prog [-M] max %missing data(uninclude) [-I] vcf \
-[-O] map file name'
-descr ='''transfer vcf file generated from freebayes into map file which used to construct
-genetic map.'''
+msg_usage = 'usage: %prog [-I] vcf [-O] map file name'
+descr ='''transfer vcf file into map file used in GenotypeCorrector.'''
 optparser = OptionParser(usage = msg_usage, description = descr)
-optparser.add_option('-M', '--min_missing', dest = 'min_missing_data', \
-default = 0.5,
-                      help = 'Maximal missing data rate of each line in \
-vcf.default is 0.5, if mr = 0.5, this line will be retained.')
 optparser.add_option('-I', '--input', dest = 'vcffile',
-                      help = 'Please input your vcf file from freebayes')
+                      help = 'input your vcf file')
 optparser.add_option('-O', '--output', dest = 'mapfile',
-                      help = 'The minimal depth of each SNP sites')
+                      help = 'output name of map file')
 options, args = optparser.parse_args()
 
-chrs = []
-def parse_locus(chr, pos):
-    if chr not in chrs:
-        chrs.append(chr)
-        print chr
-    return '%s-p%s'%(chr, pos)
 
-def parse_gt(genotype, depth):
-    if genotype == '0/0' and depth >= 3:
-        return 'A'
-    elif genotype == '0/1' and depth >= 5:
-        return 'X'
-    elif genotype == '1/1' and depth >= 3:
-        return 'B'
-    else:
-        return '-'
+parse_gt={'0|0':'A','0|1':'X','1|0':'X','1|1':'B','.|.':'-'}
 
-def judgeline(gtline, num_samp, min_missing_rate):
-        dash_rate = gtline.count('-')/float(num_samp)
-        if dash_rate > min_missing_rate:
-            return '-'
-        if dash_rate <= min_missing_rate:
-            return '\t'.join(gtline)+'\n'
-
-def fb2map(fbfile, mapfile, min_missing_rate):
-    '''min_dp should be int and min_missing_rate should be a float'''
+def fb2map(fbfile, mapfile):
     vcffile = open(fbfile)
     myvcf = vcf.Reader(vcffile)
     samples = myvcf.samples # samples list in this vcf file
@@ -58,26 +30,18 @@ def fb2map(fbfile, mapfile, min_missing_rate):
         gtline = []
         chrom = i.CHROM
         pos = str(i.POS)
-        locus = parse_locus(chrom, pos)
+        locus = '%s-%s'%(chrom, pos)
         gtline.append(locus)
         for j in i.samples:
             gt = j['GT']
-            try: dp = j['DP']
-            except:
-                print 'Sample %s. No DP value in %s %s, set as 0'%(j, chrom, pos)
-                dp = 0
-            real_gt = parse_gt(gt, dp)
+            real_gt = parse_gt[gt]
             gtline.append(real_gt)
-        lastline = judgeline(gtline, sam_num, min_missing_rate)
-        if lastline == '-':
-            pass
-        else:
-            mapfile.write(lastline)
+        line = '\t'.join(gtline)+'\n'
+        mapfile.write(line)
     mapfile.close()
     vcffile.close()
 
 if __name__ == "__main__":
-    M = float(options.min_missing_data)
     I = options.vcffile
     O = options.mapfile
-    fb2map(I, O, M)
+    fb2map(I, O)
