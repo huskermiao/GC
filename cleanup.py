a830becc10182e4e90f550db40dfa9a4d7d33990
cmiao@huskers.unl.edu <cmiao@huskers.unl.edu>
2018-06-22 14:35:15 -0500

    set default values for cleanup

--- cleanup.py
+++ cleanup.py
@@ -5,7 +5,7 @@ from optparse import OptionParser
 
 msg_usage = '''python %prog [options]
 example:
-python cleanup.py -i input_file -o output_file -a A -b B -x X -c -'''
+python cleanup.py -i input_file -o output_file'''
 descr = '''DESCRIPTION: Remove original genotypes with brackets.'''
 
 optparser = OptionParser(usage = msg_usage, description = descr)
@@ -14,18 +14,18 @@ optparser.add_option('-i', '--genotype-matrix', dest = 'matrix_filename',
                      origianl genotypes with brackets''')
 optparser.add_option('-o', '--output', dest = 'output_filename',
                      help = 'output file name.')
-optparser.add_option('-a', '--ref_gt', dest = 'ref_genotype',
+optparser.add_option('-a', '--ref_gt', dest = 'ref_genotype',default='A',
                      help = "Homozygous 0/0 genotype letter in your genotype \n\
-                     matrix file. Usually is 'a' or 'A'.")
-optparser.add_option('-b', '--alt_gt', dest = 'alt_genotype',
+                     matrix file. Default is 'A'.")
+optparser.add_option('-b', '--alt_gt', dest = 'alt_genotype',default = 'B',
                      help = "Homozygous 1/1 genotype letter in your genotype \n\
-                     matrix file. Usually is 'b' or 'B'.")
-optparser.add_option('-x', '--hetero_gt', dest = 'heterozygous_genotype',
+                     matrix file. Default is 'B'.")
+optparser.add_option('-x', '--hetero_gt', dest = 'heterozygous_genotype',default = 'X',
                      help = "Heterozygous genotype letter in your genotype \n\
-                     matrix file. Usually is 'h' or 'X'.")
-optparser.add_option('-c', '--miss_gt', dest = 'missing_data_character',
+                     matrix file. Default is'X'.")
+optparser.add_option('-c', '--miss_gt', dest = 'missing_data_character',default = '-',
                      help = "Missing data character in your genotype \n\
-                     matrix file. Usually is '-' or '.'.")
+                     matrix file. Default is '-'.")
 options, args = optparser.parse_args()
 
 def cleanup(correctedfile,output,A,B,X,C):
